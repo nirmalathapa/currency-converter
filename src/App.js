7ba3ee8dbd70580189ed1d16458cd09ef1c35585
nirmala <nirmala@surf.net.np>
2019-06-23 18:40:32 +0200

    convert functional component to statful class component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <form>
-        <label>
-          Amount:
-          <input type="number" />
-        </label>
-        <label>
-          From:
-          <select>
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-          </select>
-        </label>
-        <label>
-          To:
-          <select>
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-          </select>
-        </label>
-        <input type="submit" value="Convert" />
-      </form>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: "EUR",
+      to: "EUR",
+      amount: 1
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <label>
+            Amount:
+            <input type="number" />
+          </label>
+          <label>
+            From:
+            <select>
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+            </select>
+          </label>
+          <label>
+            To:
+            <select>
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+            </select>
+          </label>
+          <input type="submit" value="Convert" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
