b217c9bd2a637a19a64dea085c317e7558529c3b
nirmala <nirmala@surf.net.np>
2019-06-23 19:27:16 +0200

    use local env for api key

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://data.fixer.io/api/";
-const API_KEY = "ea822fdcd3ab8bf29d1685c349225ad9";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export async function getSymbols() {
   const response = await axios.get(`${BASE_URL}symbols`, {
